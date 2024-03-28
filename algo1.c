7274092994289997170fad2f7f9c441640f50189
Anas Amellah <anamella@e2r8p6.1337.ma>
2024-03-28 23:20:05 +0000

    test

--- algo1.c
+++ algo1.c
@@ -136,9 +136,109 @@ void sort_algo_1(stack **stack_a, stack **stack_b, int val)
 
 stack *get_best_move(stack *stack_a, stack *node, int val)
 {
-    get_best_move_from_top();
-}
-get_best_move_from_top(stack *stack_a, int val)
-{
     
 }
+int get_best_move_from_top(stack *stack_a, int val)
+{
+    int i;
+
+    i = 0;
+    while (stack_a != NULL)
+    {
+        if (stack_a->data <= val)
+            return (i);
+        stack_a = stack_a->next;
+        i++
+    }
+    return (0);
+}
+int get_best_move_from_last(stack *stack_a, int val)
+{
+    stack *tmp;
+    int i;
+    int index;
+
+    index = 0;
+    i = link_len(stack_a);
+    tmp = stack_a;
+    while (i >= 0)
+    {
+        index = 0;
+       while (index <= i && tmp != NULL)
+        {
+            tmp = tmp->next;
+            index++;
+        }
+        if (tmp->data <= val)
+            return (i);
+        tmp = stack_a;
+        i--;
+    }
+    return (0); 
+}
+
+
+
+// #include <stdlib.h>
+// typedef struct t_stack
+// {
+//   int data;
+//   struct t_stack *next;
+// }stack;
+// int link_len(stack *stack_t)
+// {
+//     int i;
+
+//     i = 0;
+//     while (stack_t != NULL)
+//     {
+//         stack_t = stack_t->next;
+//         i++;
+//     }
+//     return (i);
+// }
+// int get_best_move_from_last(stack *stack_a, int val)
+// {
+//     stack *tmp;
+//     int i;
+//     int index;
+
+//     index = 0;
+//     i = link_len(stack_a);
+//     tmp = stack_a;
+//     while (i >= 0)
+//     {
+//         index = 0;
+//        while (index <= i && tmp->next != NULL)
+//         {
+//             tmp = tmp->next;
+//             index++;
+//         }
+//         if (tmp->data <= val)
+//             return (i);
+//         tmp = stack_a;
+//         i--;
+//     }
+//     return (0); 
+// }
+// int main() {
+  
+//   stack *stack_a;
+//   stack * s1 = malloc(sizeof(stack));
+//   stack * s2 = malloc(sizeof(stack));
+//   stack * s3 = malloc(sizeof(stack));
+//   stack * s4 = malloc(sizeof(stack));
+  
+//   stack_a = s1;
+//   s1->data = 10;
+//   s1->next = s2;
+//   s2->data = -3;
+//   s2->next = s3;
+//   s3->data = 1;
+//   s3->next = s4;
+//   s4->data = 556;
+//   s4->next = NULL;
+  
+//   int a = get_best_move_from_last(stack_a, 2);
+//   return 0;
+// }
